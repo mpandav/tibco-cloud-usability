c4c702efcb1ea1411d83d743a500c9bf7ef70814
milnd pandav <38240734+mpandav@users.noreply.github.com>
2023-11-11 14:19:42 +0100

    Update build-agent.sh

--- TCI/tibagent/Hybrid_Connectivity/docker-agent-hybrid-connectivity/single-dockerfile/build-agent.sh
+++ TCI/tibagent/Hybrid_Connectivity/docker-agent-hybrid-connectivity/single-dockerfile/build-agent.sh
@@ -3,6 +3,7 @@
 echo ================================================================
 echo
 echo "Login to TIBCO Cloud using token [${ACCESS_TOKEN}]..."
+echo  "./tibagent authorize --token ${ACCESS_TOKEN}" 
 echo
 echo ================================================================
 
@@ -11,6 +12,7 @@ echo ================================================================
 echo ================================================================
 echo
 echo "Configure  tibagent [${AGENT_NAME}:${AGENT_PORT}]..."
+echo "./tibagent configure agent --port ${AGENT_PORT} --config-dir . ${AGENT_NAME}"
 echo
 echo ================================================================
 
@@ -19,6 +21,7 @@ echo ================================================================
 echo ================================================================
 echo
 echo "Configure Connectivity for tibagent [${AGENT_NAME}:${AGENT_PORT} and ${AGENT_SPEC}]..."
+echo " ./tibagent configure connect --config-dir . --accessSecret ${ACCESS_SECRET}  --accessKey ${ACCESS_KEY_NAME}  ${AGENT_NAME}"
 echo
 echo ================================================================
 
